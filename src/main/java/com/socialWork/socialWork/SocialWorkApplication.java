f41e912130a07a9143a33ad1f9e33cd6353f6dde
hevinhsu <57029923+hevinhsu@users.noreply.github.com>
2020-08-08 00:09:10 +0800

    first commit

---
+++ src/main/java/com/socialWork/socialWork/SocialWorkApplication.java
@@ -0,0 +1,13 @@
+package com.socialWork.socialWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialWork/socialWork/SocialWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialWork.socialWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
