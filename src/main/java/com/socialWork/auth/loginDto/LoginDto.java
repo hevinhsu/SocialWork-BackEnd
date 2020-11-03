ad5acdd1c94f2b741455f0429602ce685d6d89c8
hevinhsu <57029923+hevinhsu@users.noreply.github.com>
2020-11-03 15:15:35 +0800

    Dto新增中文參數說明

--- src/main/java/com/socialWork/auth/loginDto/LoginDto.java
+++ src/main/java/com/socialWork/auth/loginDto/LoginDto.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.validation.constraints.NotBlank;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
@@ -15,7 +16,9 @@ public class LoginDto implements Serializable {
 	 */
 	private static final long serialVersionUID = -3157279962216631429L;
 	@NotBlank(message = "使用者名稱不能為空")
+	@ApiModelProperty(value = "帳號", position = 1)
 	private String username;
+	@ApiModelProperty(value = "密碼", position = 2)
 	@NotBlank(message = "密碼不能為空")
 	private String password;
 }
